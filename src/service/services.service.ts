49f5e80bd455993832030ca24f7449176b82c39b
Christina <khryschristinaglinkina90@gmail.com>
2026-09-18 18:09:02 +0200

    feat: complete services module with CRUD

--- src/service/services.service.ts
+++ src/service/services.service.ts
@@ -4,6 +4,8 @@ import { ServicesMapper } from './dto/service.mapper';
 import { ServiceDto } from './dto/service.dto';
 import { Service } from './service.entity';
 import { ServiceSaveDto } from './dto/service.save-dto';
+import { ServiceUpdateDto } from './dto/service.update-dto';
+
 
 
 
@@ -20,16 +22,32 @@ export class ServicesService {
     await this.repository.save(entity);
     return this.mapper.mapEntityToDto(entity);
   }
-  async getActiveServicesById(id: number): Promise<ServiceDto> {
+  async getAllActiveServices(): Promise<ServiceDto[]> {
+    const service: Service[] = await this.repository.findAllActive();
+    return this.mapper.mapEntityListToDtoList(service);
+  }
+
+  async getActiveServiceById(id: number): Promise<ServiceDto> {
     const service: Service = await this.getActiveEntityById(id);
 
     return this.mapper.mapEntityToDto(service);
   }
+
   private async getActiveEntityById(id: number): Promise<Service> {
     const service: Service | null = await this.repository.findById(id);
     if (!service || !service.isActive) {
       throw new Error(`Service with id ${id} not found`);
     }
     return service;
+  }
+  async update(id: number, updateDto: ServiceUpdateDto): Promise<void> {
+    const service = await this.getActiveEntityById(id);
+    service.name = updateDto.newName;
+    await this.repository.save(service);
+  }
+  async delete(id: number): Promise<void> {
+    const service = await this.getActiveEntityById(id);
+    service.isActive = false;
+    await this.repository.save(service);
   }
 }
